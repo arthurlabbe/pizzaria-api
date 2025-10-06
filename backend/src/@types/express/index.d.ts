1cd662717ca565b1e579ff374afd836886e0e5ea
ArthurLabbê <arthurlabbe@hotmail.com>
2025-10-06 16:42:38 -0300

    Primeira Versão

---
+++ backend/src/@types/express/index.d.ts
@@ -0,0 +1,12 @@
+// src/@types/express/index.d.ts
+
+declare global {
+  namespace Express {
+    interface Request {
+      user_id: string;
+    }
+  }
+}
+
+// torna o arquivo um módulo e evita conflitos de escopo
+export {};
